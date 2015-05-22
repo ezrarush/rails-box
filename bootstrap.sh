7841e078b8ee23713846548fe6d6bfac6a2e6868
Ezra Rush <rushwest@gmail.com>
2015-05-21 20:46:58 -0500

    ruby repository and emacs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ function install {
     apt-get -y install "$@" >/dev/null 2>&1
 }
 
-#echo updating package information
-#apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
+echo updating package information
+apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
 
 install 'development tools' build-essential
@@ -25,5 +25,7 @@ install PostgreSQL postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser --superuser vagrant
 
 install 'ExecJS runtime' nodejs
+
+install Emacs emacs24
 
 echo 'all set, rock on!'
